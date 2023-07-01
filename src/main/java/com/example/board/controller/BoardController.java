8a447f199392aef5cb8251839f65b37bef32dd06
Sangmin <101931428+netco97@users.noreply.github.com>
2023-07-01 16:50:28 +0900

    Board Save

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,25 @@
+package com.example.board.controller;
+
+import com.example.board.dto.BoardDTO;
+import com.example.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequiredArgsConstructor //생성자 주입으로 의존성 주입
+@RequestMapping("/board") //컨트롤러가 우선 //board 주소를 매핑받음
+public class BoardController {
+    private final BoardService boardService;
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO){
+        System.out.println("boardDTO = " + boardDTO);
+        boardService.save(boardDTO);
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/board/service/BoardService.java
@@ -0,0 +1,20 @@
+package com.example.board.service;
+
+import com.example.board.dto.BoardDTO;
+import com.example.board.entity.BoardEntity;
+import com.example.board.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+// DTO -> Entity (Entity Class)
+// Entity -> DTO (DTO Class)
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+    public void save(BoardDTO boardDTO) {
+        BoardEntity boardEntity = BoardEntity.toSaveEntity(boardDTO);
+        boardRepository.save(boardEntity); //insert 쿼리 작동(JPA의 Repository.save)
+    }
+}
