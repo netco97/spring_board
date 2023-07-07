1efb48482d8caffbfef5991cad2ba87ca1a5aa1a
Sangmin <101931428+netco97@users.noreply.github.com>
2023-07-08 01:07:55 +0900

    파일첨부

---
+++ src/main/java/com/example/board/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.example.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private String resourcePath = "/upload/**"; //view 에서 접근 경로
+    private String savePath = "file:///Users/isangmin/springboot_img/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler(resourcePath).addResourceLocations(savePath);
+    }
+}
